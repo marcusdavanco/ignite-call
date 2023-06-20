a8eea4290bcf9b42d992ac718feb883bc576d9cb
Marcus Davanço <md.daves@gmail.com>
2023-06-20 15:00:19 -0300

    feat: Instalação do Design System

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
-import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { Roboto } from 'next/font/google'
+import { Heading } from '@ignite-ui/react'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin']
+})
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Ignite Call</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${roboto.className}`}>
+        <Heading as='h1'>Hello World</Heading>
+      </main>
+    </>
   )
 }
