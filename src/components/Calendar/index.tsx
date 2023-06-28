628f67b18b57aaf910d710aac4526b162e8a1482
Marcus Davanço <md.daves@gmail.com>
2023-06-28 08:13:06 -0300

    feat: Carregando horários disponíveis

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -99,7 +99,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           })
         }
 
-        return weeks.filter(array => array.days.length === 7)
+        return weeks.filter((array) => array.days.length === 7)
       },
       [],
     )
