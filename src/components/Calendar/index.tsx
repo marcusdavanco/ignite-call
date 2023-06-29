ef9e75f5dfec233491bc7cc1436f316539a38505
Marcus Davanço <md.daves@gmail.com>
2023-06-29 05:20:23 -0300

    feat: Alterando banco para MySQL

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -68,6 +68,10 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   })
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -105,7 +109,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
         return {
           date,
           disabled: date.endOf('day').isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get('day'))
+            blockedDates.blockedWeekDays.includes(date.get('day'))
         }
       }),
       ...nextMonthFillArray.map((date) => {
